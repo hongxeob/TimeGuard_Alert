2b7e7b1c4a14c11feb8bf1a9ddf3edde53f085d5
Hongseob Lee <sjun2918@naver.com>
2023-08-18 16:51:54 +0900

    :tada: 프로젝트 초기 세팅

---
+++ src/main/java/project/timeguardalert/TimeGuardAlertApplication.java
@@ -0,0 +1,13 @@
+package project.timeguardalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeGuardAlertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeGuardAlertApplication.class, args);
+    }
+
+}
