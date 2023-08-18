8f95817892c00a4edf9397535fda2a5d277ca739
Hongseob Lee <sjun2918@naver.com>
2023-08-19 03:34:21 +0900

    :wrench: 누락된 파일 커밋

---
+++ src/test/java/project/timeguardalert/TimeGuardAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package project.timeguardalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeGuardAlertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
