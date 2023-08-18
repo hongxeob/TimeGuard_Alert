f02479a9eeb793624d806f6a9351c7e0518871b3
Hongseob Lee <sjun2918@naver.com>
2023-08-19 03:28:39 +0900

    :hammer: 깃 로그 삭제

--- src/main/java/project/timeguardalert/application/dto/NotificationResponse.java
+++
@@ -1,4 +0,0 @@
-package project.timeguardalert.application.dto;
-
-public record NotificationResponse(boolean isUnitSaleDay, String unitStartTime, int unitBookingCount) {
-}
--- src/main/java/project/timeguardalert/domain/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package project.timeguardalert.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-
-}
