8a646152f979107d2f1e2fbe4d1ac6d62af9c683
Hongseob Lee <sjun2918@naver.com>
2023-08-18 20:27:10 +0900

    :sparkles: Notification 도메인 구성

---
+++ src/main/java/project/timeguardalert/domain/NotificationRepository.java
@@ -0,0 +1,7 @@
+package project.timeguardalert.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+}
