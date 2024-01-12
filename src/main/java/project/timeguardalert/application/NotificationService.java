510cbb7abd1a2271d3a21b7d0fe4516a61179855
Hongseob Lee <sjun2918@naver.com>
2024-01-12 16:44:34 +0900

    refactor: JPA 코드 제거

--- src/main/java/project/timeguardalert/application/NotificationService.java
+++ src/main/java/project/timeguardalert/application/NotificationService.java
@@ -2,24 +2,21 @@ package project.timeguardalert.application;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import project.timeguardalert.application.dto.NotificationResponse;
 import project.timeguardalert.infrastructure.NotificationFeignClient;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class NotificationService {
 
 	private final NotificationFeignClient notificationFeignClient;
 	private final UserInputData userInputData;
-	private final Logger log = LoggerFactory.getLogger(getClass());
 
-	@Transactional(readOnly = true)
 	public List<NotificationResponse> getHourlySchedules() {
 		String placeNumber = userInputData.getPlaceNumber();
 		String placeOptionNumber = userInputData.getPlaceOptionNumber();
